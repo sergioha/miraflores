81a126591bc4fbd7f60d0b5266e9e48d56408d1e
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2012-12-21 23:07:11 -0400

    fabric: iniciando configuracion del servidor remoto

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,11 @@
-from fabric.api import env, require, local, cd, lcd, hosts, sudo, run
+from fabric.api import settings, env, require, local, cd, lcd, hosts, sudo, run
 from fabric.contrib import django
+
 try:
-        from fabsettings import WF_HOST, PROJECT_NAME, REPOSITORY, USER, PASSWORD
+    from fabsettings import WF_HOST, PROJECT_NAME, REPOSITORY, USER, PASSWORD
 except ImportError:
-        print "ImportError: Couldn't find fabsettings.py, it either does not exist or giving import problems (missing settings)"
-        sys.exit(1)
+    print "ImportError: Couldn't find fabsettings.py, it either does not exist or giving import problems (missing settings)"
+    sys.exit(1)
 
 django.project('miraflores')
 env.hosts = [WF_HOST]
@@ -14,22 +15,27 @@ env.project = PROJECT_NAME
 env.repo = REPOSITORY
 env.home = "/home/%s" % USER
 env.project_dir = env.home + '/webapps/' + PROJECT_NAME
-env.project_dir_env = env.project_dir + PROJECT_NAME
+env.project_dir_env = env.project_dir + '/' + PROJECT_NAME
 
 def deploy_templates():
     local("git push origin master")
     deploy_server()
 
 def deploy_server():
+    configure_server()
+    is_repo_clean()
     with cd(env.project_dir_env):
         run("git pull")
     with cd(env.project_dir):
-        run(". apache/bin/restart")
+        run(". apache2/bin/restart")
 
-def is_local_repo_clean(): """ Checks if there are uncommitted changes in the local git repository. """ 
-    with settings(warn_only=True): 
+def is_local_repo_clean():
+    with settings(warn_only=True):
         return local("git status 2>&1|grep 'nothing to commit' > /dev/null").succeeded
 
-def is_repo_clean(): """ Checks if there are uncommitted changes in the remote git repository. """ 
-    with settings(warn_only=True): return run("git status 2>&1|grep 'nothing to commit' > /dev/null").succeeded
+def is_repo_clean():
+    with settings(warn_only=True):
+        return run("git status 2>&1|grep 'nothing to commit' > /dev/null").succeeded
 
+def configure_server():
+    run("source ~/.bashrc")
