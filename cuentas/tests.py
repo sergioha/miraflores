d6ad83ff1dc6c2c1c7be3536211db0f265809f7b
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2012-12-25 22:19:46 -0400

    unit tests: agregando configuracion tests.py para automatizar corrida de tests y crear la app tests para el testeo de nuestro portal con sus propias configuraciones excluyendo terceras apps

--- cuentas/tests.py
+++ cuentas/tests.py
@@ -5,15 +5,17 @@ from django.contrib.auth.models import User
 class PerfilClienteTest(TestCase):
 
     def setUp(self):
-        self.usuario1 = User.objects.create('test1','sergio.hinojosa.avila@gmail.com','123456')
-        self.admin = User.objects.create('admintest','ventas@npneumatica.com','123456')
-        self.admin.is_staff = True
+        self.usuario1 = User.objects.create()
+        self.usuario1.email = 'sergio.hinojosa.avila@gmail.com'
+        self.usuario1.set_password ='123456'
+        #self.admin = User.objects.create('admintest','ventas@npneumatica.com','123456')
+        #self.admin.is_staff = True
         self.usuario1.save()
-        self.admin.save()
+        #self.admin.save()
 
     def test_agregar_nuevo_perfil(self):
         """
         Agregamos un nuevo perfil con toda la informacion requerida
         """
-        self.usuario.perfil
+        #self.usuario.perfil
         self.assertEqual(1 + 1, 2)
---
+++ tests.py
@@ -0,0 +1,41 @@
+"""
+python tests.py
+
+"""
+import sys
+import os
+
+os.environ["DJANGO_SETTINGS_MODULE"] = 'miraflores.settings'
+from miraflores import settings
+
+settings.DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/sergio/Desktop/miraflores/devMiraflores/miraflores/dbtest.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+
+settings.INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.sites',
+    'cuentas',
+    'tests',
+)
+settings.TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+
+def main():
+    from django.test.utils import get_runner
+    test_runner = get_runner(settings)(interactive = False)
+    failures = test_runner.run_tests(['cuentas','tests',])
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    main()
+
