93323972c86e3ce5a8816c51c0661c08a99f29d6
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2012-12-26 01:46:36 -0400

    Tests Cuentas Models: creacion de los tests para el modelo de clientes

--- tests/cuentas/tests_models.py
+++ tests/cuentas/tests_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from cuentas.models import *
 
 class CuentaClienteNuevoTestCase(TestCase):
     
@@ -9,9 +10,16 @@ class CuentaClienteNuevoTestCase(TestCase):
         administrador y cuentas de usuario del tipo
         cliente que seran usadas en los tests.
         """
-        self.user_info_1 = {'username': 'julio',
-                       'password': '123456',
-                       'email': 'julio@lavanderiamiraflores.com'}
+        self.user_info_1 = {'nombre': 'julio',
+                'apellidos': 'medrano',
+                'direccion': 'lorem ipsum asimet duo',
+                'ciudad': 'Cochabamba',
+                'telefono': '4414444',
+                'celular': '77777777',
+                'tipo_empresa': 'Unipersonal',
+                'servicio_requerido': '1',
+                'servicio_preferencia': '1',}
+        self.email_1 = 'julio@lavanderiamiraflores.com'
         self.user_info_2 = {'username': 'sergio',
                        'password': '123456',
                        'email': 'sergio@lavanderiamiraflores.com'}
@@ -27,5 +35,6 @@ class CuentaClienteNuevoTestCase(TestCase):
     def test_crear_nuevo_cliente(self):
         """
         """
-        self.assertEqual(self.admin, User.objects.get(username='admin'))
+        self.cliente_1 = Cliente.objects.create_user(email = self.email_1, password = '123456', **self.user_info_1)
+        self.assertEqual(self.cliente_1.usuario_cliente, Cliente.objects.get(usuario__username = self.email_1))
 
