97de586c3c4fd560b7193dab081773b2d83c5d5f
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2012-12-29 21:54:17 -0400

    unittesting:: clientes: testeando modelo cliente crear nuevo cliente

--- unittesting/tests.py
+++ unittesting/tests.py
@@ -3,18 +3,24 @@ from clientes.models import Cliente
 
 class CuentaClienteNuevoTestCase(TestCase):
     
+    def create_client(**kwargs):
+        defaults = {
+        "likes_cheese": True,
+        "age": 32,
+        "address": "3815 Brookside Dr",
+        }
+        defaults.update(kwargs)
+        if "user" not in defaults:
+            defaults["user"] = create_user()
+        return Profile.objects.create(**defaults)
+
     def setUp(self):
         """
         Inicializaremos los datos de cuentas de usuarios con rol de
         administrador y cuentas de usuario del tipo
         cliente que seran usadas en los tests.
         """
-        self.user_info_1 = {'ci':'2121542',
-                'contrasena' : '123456',
-                'nombres': 'julio',
-                'apellidos': 'medrano',
-                'email' : 'julio@lavanderiamiraflores.com',
-                'telefono': '4414444',
+        self.user_info_1 = {
                 'celular': '77777777',
                 'direccion': 'lorem ipsum asimet duo',
                 'ciudad': 'Cochabamba',
@@ -25,6 +31,11 @@ class CuentaClienteNuevoTestCase(TestCase):
                 'produccion' :1000,
                 'servicio_requerido': '1',
                 'servicio_preferencia': '1',}
+        self.ci_1 = '2121542'
+        self.email_1 = 'julio@lavanderiamiraflores.com'
+        self.nombres_1 ='julio'
+        self.apellidos_1 = 'medrano'
+        self.telefono_1 = 4414444
         self.user_info_2 = {'username': 'sergio',
                        'password': '123456',
                        'email': 'sergio@lavanderiamiraflores.com'}
@@ -35,6 +46,10 @@ class CuentaClienteNuevoTestCase(TestCase):
     def test_crear_nuevo_cliente(self):
         """
         """
-        self.cliente_1 = Cliente.objects.create(**self.user_info_1)
+        self.cliente_1 = Cliente.objects.create_user(self.ci_1, self.email_1, self.nombres_1, self.apellidos_1, self.telefono_1, password = '123456', **self.user_info_1)
         self.assertEqual(self.cliente_1, Cliente.objects.get(ci = self.cliente_1.ci))
+    
+    def test_verificar_contrasena(self):
+        self.cliente_1 = Cliente.objects.create_user(self.ci_1, self.email_1, self.nombres_1, self.apellidos_1, self.telefono_1, password = '123456', **self.user_info_1)
+        self.assertTrue(self.cliente_1.verificar_contrasena('123456'))
 
