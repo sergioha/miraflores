548983b2093ec5963533ed3cf8af880dd1f00454
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2013-01-02 16:30:34 -0400

    functionaltesting:: servicios: test crear nuevo servicio en zonacliente

--- functionaltesting/tests.py
+++ functionaltesting/tests.py
@@ -2,27 +2,31 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class ClienteAdminTestSuite(LiveServerTestCase):
-
-    fixtures = ['clientes.json']
-
+class BasicTestSuite(LiveServerTestCase):
+    
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
-    #todo terminar el test
-    def test_crear_nuevo_cliente(self):
+
+    def _ingresar_como_admin(self):
         self.browser.get(self.live_server_url + '/admin/')
         admin_text = self.browser.find_element_by_id('login-form')
-        self.assertIn('Usuario:',admin_text.text)
-
+        #self.assertIn('Usuario:',admin_text.text)
         username = self.browser.find_element_by_name('username')
         username.send_keys('admin')
         password = self.browser.find_element_by_name('password')
         password.send_keys('123456')
         password.send_keys(Keys.RETURN)
+
+class ClienteAdminTestSuite(BasicTestSuite):
+
+    fixtures = ['clientes.json']
+    
+    def test_crear_nuevo_cliente(self):
+        self._ingresar_como_admin()
         #Pagina principal del admin donde estan todas las aplicaciones
         clientes_link = self.browser.find_element_by_link_text('Cuentas de Clientes')
         self.assertEquals(clientes_link.text,u'Cuentas de Clientes')
@@ -34,16 +38,9 @@ class ClienteAdminTestSuite(LiveServerTestCase):
         nuevo_cliente_link.click()
         clientes_link = browser.find_element_by_link_text('Cuentas de Clientes')
         
-class ClienteTestSuite(LiveServerTestCase):
+class ClienteTestSuite(BasicTestSuite):
 
     fixtures = ['clientes.json']
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(5)
-
-    def tearDown(self):
-        self.browser.quit()
 
     def test_crear_nuevo_cliente_con_campos_requeridos(self):
         self.browser.get(self.live_server_url + '/zonaclientes/registro/')
@@ -58,4 +55,14 @@ class ClienteTestSuite(LiveServerTestCase):
         
         #Pagina principal del cliente
         body = self.browser.find_element_by_tag_name('body')
+
+class ServicioNuevoTestSuite(BasicTestSuite):
+
+    fixtures = ['clientes.json']
+    
+    def test_crear_nuevo_servicio(self):
+        self._ingresar_como_admin()
+        link_servicios = self.browser.find_element_by_link_text('Servicios')
+        link_servicios.click()
+        
         
