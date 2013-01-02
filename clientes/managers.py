3db9a3918526a30b3012fa9d8294c16eea21522c
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2013-01-02 16:29:39 -0400

    unitesting :: clientes: refactoring unitestes

--- clientes/managers.py
+++ clientes/managers.py
@@ -4,13 +4,12 @@ from django.utils import timezone
 from django.contrib.auth.hashers import check_password, make_password
 
 class ClienteManager(models.Manager):
-    def create_user(self, ci, email=None, nombres=None, apellidos=None, telefono=None, password=None, **extra_fields):
+    def create_user(self, ci, password, **extra_fields):
         now = timezone.now()
     #Crea una cuenta de cliente con su ci, nombres, apellidos, password y telefono. """
         if not ci:
             raise ValueError('El usuario debe registrar su documento de identidad')
-        user = self.model(ci=ci, email=email, nombres=nombres, apellidos=apellidos, telefono=telefono,
-                          is_active=False, date_joined=now, **extra_fields)
+        user = self.model(ci=ci, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
--- unittesting/tests.py
+++ unittesting/tests.py
@@ -1,18 +1,33 @@
 from django.test import TestCase
 from clientes.models import Cliente
 
-class CuentaClienteNuevoTestCase(TestCase):
+class ClienteTestCase(TestCase):
     
-    def create_client(**kwargs):
+    ci_1 = '2121542'
+    password = '132465'
+    
+    def _crear_cuenta_cliente(self, ci, password, **kwargs):
         defaults = {
-        "likes_cheese": True,
-        "age": 32,
-        "address": "3815 Brookside Dr",
+            'nombres' : 'julio',
+            'apellidos' : 'medrano',
+            'email' : 'julio@lavanderiamiraflores.com',
+            'telefono' : '4414444',
+            'celular': '77777777',
+            'direccion': 'lorem ipsum asimet duo',
+            'ciudad': 'Cochabamba',
+            'nombre_empresa' : 'las lomas',
+            'tipo_empresa': 'Unipersonal',
+            'numero_empleados' : 10,
+            'productos' :'chamarras, jeans',
+            'produccion' :1000,
+            'servicio_requerido': '1',
+            'servicio_preferencia': '1',
         }
         defaults.update(kwargs)
-        if "user" not in defaults:
-            defaults["user"] = create_user()
-        return Profile.objects.create(**defaults)
+        #if "user" not in defaults:
+        #    defaults["user"] = create_user()
+        #return Profile.objects.create(**defaults)
+        return Cliente.objects.create_user(ci, password, **defaults)
 
     def setUp(self):
         """
@@ -20,36 +35,14 @@ class CuentaClienteNuevoTestCase(TestCase):
         administrador y cuentas de usuario del tipo
         cliente que seran usadas en los tests.
         """
-        self.user_info_1 = {
-                'celular': '77777777',
-                'direccion': 'lorem ipsum asimet duo',
-                'ciudad': 'Cochabamba',
-                'nombre_empresa' : 'las lomas',
-                'tipo_empresa': 'Unipersonal',
-                'numero_empleados' : 10,
-                'productos' :'chamarras, jeans',
-                'produccion' :1000,
-                'servicio_requerido': '1',
-                'servicio_preferencia': '1',}
-        self.ci_1 = '2121542'
-        self.email_1 = 'julio@lavanderiamiraflores.com'
-        self.nombres_1 ='julio'
-        self.apellidos_1 = 'medrano'
-        self.telefono_1 = 4414444
-        self.user_info_2 = {'username': 'sergio',
-                       'password': '123456',
-                       'email': 'sergio@lavanderiamiraflores.com'}
-        self.user_info_3 = {'username': 'ronald',
-                       'password': '123456',
-                       'email': 'ronald@lavanderiamiraflores.com'}
+        
 
-    def test_crear_nuevo_cliente(self):
+    def test_crear_nueva_cuenta_cliente(self):
         """
         """
-        self.cliente_1 = Cliente.objects.create_user(self.ci_1, self.email_1, self.nombres_1, self.apellidos_1, self.telefono_1, password = '123456', **self.user_info_1)
+        self.cliente_1 = self._crear_cuenta_cliente(self.ci_1, self.password)
         self.assertEqual(self.cliente_1, Cliente.objects.get(ci = self.cliente_1.ci))
     
     def test_verificar_contrasena(self):
-        self.cliente_1 = Cliente.objects.create_user(self.ci_1, self.email_1, self.nombres_1, self.apellidos_1, self.telefono_1, password = '123456', **self.user_info_1)
+        self.cliente_1 = self._crear_cuenta_cliente(self.ci_1, self.password)
         self.assertTrue(self.cliente_1.verificar_contrasena('123456'))
-
