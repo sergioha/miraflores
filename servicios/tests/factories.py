b444fc04dbd1347d0051f2f52198c83d38476539
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2013-01-09 15:35:38 -0400

    factory: overriding prepare method for serviciofactory to create 3 objets of listapreciosfactory

--- servicios/tests/factories.py
+++ servicios/tests/factories.py
@@ -1,5 +1,6 @@
+import random
 import factory
-from servicios.models import TipoServicio, Servicio
+from servicios.models import TipoServicio, Servicio, ListaPrecios, TALLAS
 
 class TipoServicioFactory(factory.Factory):
     FACTORY_FOR = TipoServicio
@@ -9,4 +10,24 @@ class TipoServicioFactory(factory.Factory):
 class ServicioFactory(factory.Factory):
     FACTORY_FOR = Servicio
     tipo_servicio = factory.SubFactory(TipoServicioFactory)
+    nombre = factory.Sequence(lambda n: 'Servicio{0}'.format(n))
+    
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        lista = kwargs.pop('lista',None)
+        servicio = super(ServicioFactory, cls)._prepare(create, **kwargs)
+        if lista:
+            ListaPreciosFactory(servicio=servicio, talla=1)
+            ListaPreciosFactory(servicio=servicio, talla=2)
+            ListaPreciosFactory(servicio=servicio, talla=3)
+        return servicio
+    
+class ListaPreciosFactory(factory.Factory):
+    FACTORY_FOR = ListaPrecios
+    servicio = factory.SubFactory(ServicioFactory)
+    talla =  random.randint(1,3)
+    precio_bolivianos = round(random.uniform(0,2),2)
+    
+    
+    
     
