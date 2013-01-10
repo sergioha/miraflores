1c9437c9cb44e84c020c04fff145417bad06620e
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2013-01-10 18:06:36 -0400

    unitesting: servicios: fixing bugs and adding tests

--- servicios/tests/factories.py
+++ servicios/tests/factories.py
@@ -1,10 +1,16 @@
 import random
 import factory
-from servicios.models import TipoServicio, Servicio, ListaPrecios, TALLAS
+
+from django.utils import timezone
+
+from servicios.models import *
+from clientes.tests.factories import ClienteFactory
+
+CAPACIDAD = 500
 
 class TipoServicioFactory(factory.Factory):
     FACTORY_FOR = TipoServicio
-    titulo = factory.Sequence(lambda n: 'Tipo de Servicio {0}'.format(n))
+    nombre = factory.Sequence(lambda n: 'Tipo de Servicio {0}'.format(n))
     capacidad = factory.Sequence(lambda n: 3*n)
     
 class ServicioFactory(factory.Factory):
@@ -28,6 +34,42 @@ class ListaPreciosFactory(factory.Factory):
     talla =  random.randint(1,3)
     precio_bolivianos = round(random.uniform(0,2),2)
     
+contador = 0
+
+class OrdenFactory(factory.Factory):
+    FACTORY_FOR = Orden
+    cliente = factory.SubFactory(ClienteFactory)
+    fecha_entrega = timezone.now()
+    cantidad = random.randint(100,CAPACIDAD)
+    talla = random.randint(1,TALLAS.__len__()-1)
     
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        detalle = kwargs.pop('detalle',None)
+        orden = super(OrdenFactory, cls)._prepare(create, **kwargs)
+        if detalle:
+            DetalleOrdenFactory(orden=orden)
+            DetalleOrdenFactory(orden=orden)
+            DetalleOrdenFactory(orden=orden)
+        global contador
+        contador = 0
+        return orden
+
+class DetalleOrdenFactory(factory.Factory):
+    FACTORY_FOR = DetalleOrden
+    orden = factory.SubFactory(OrdenFactory)
+    fecha_ejecucion = timezone.now()
     
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        global contador
+        if 'servicio' in kwargs:
+            detalleorden = super(DetalleOrdenFactory, cls)._prepare(create, **kwargs)
+            return detalleorden
+        servicio = Servicio.objects.all()[contador]
+        contador+=1
+        kwargs['servicio'] = servicio
+        detalleorden = super(DetalleOrdenFactory, cls)._prepare(create, **kwargs)
+        return detalleorden
+
     
--- servicios/tests/models.py
+++ servicios/tests/models.py
@@ -1,30 +1,38 @@
+import datetime
+
 from django.utils import timezone
 from django.test import TestCase
-from servicios.models import TipoServicio, Servicio, ListaPrecios, TALLAS
-from servicios.tests.factories import TipoServicioFactory, ServicioFactory, ListaPreciosFactory
 from django.db import DatabaseError
+
+from servicios.models import *
+from servicios.tests.factories import *
+
 
 class ServiciosTestSuite(TestCase):
     
     contador_tipos = 0
     tipo_servicio = None
+    capacidad_lavado = CAPACIDAD
+    servicio_estoneado = None
     
     def setUp(self):
         self.tipo_servicio = TipoServicioFactory()
-        self.contador_tipos+=1
+        self.contador_tipos+=2
+        self.servicio_estoneado = ServicioFactory(nombre='estoneado', lista=True)
     
     def test_crear_nuevo_tipo_servicio(self):
         tipo_servicio_1 = TipoServicioFactory()
         tipos = TipoServicio.objects.all()
         print 'Verifica que el numero de tipo de servicios creados este correcto'
-        self.assertEqual(len(tipos),self.contador_tipos+1)#verificamos el nuevo registro contando
+        contador = self.contador_tipos +1
+        self.assertEqual(len(tipos),contador)#verificamos el nuevo registro contando
         print 'Verifica que el tipo de servicios creado se encuentre almacenado en la base de datos'
         self.assertIn(tipo_servicio_1, tipos, msg="El tipo de servicio no fue creado")
    
     def test_tipo_servicio_titulo_no_valido(self):
         print 'Verifica que nos lanze un error en la base de datos al crear un tipo de servicio con un titulo ya existente.'
         with self.assertRaises(DatabaseError):
-            TipoServicioFactory(titulo=self.tipo_servicio.titulo)
+            TipoServicioFactory(nombre=self.tipo_servicio.nombre)
 
     def test_tipo_servicio_capacidad_no_valida(self):
         print 'Verifica que nos lanze un error en la base de datos al ingresar una capacidad grande'
@@ -36,20 +44,11 @@ class ServiciosTestSuite(TestCase):
 
     def test_crear_nuevo_servicio(self):
         print 'Verifica la creacion correcta de un servicio'
-        servicio = ServicioFactory(nombre = 'estoneado')
         self.assertEqual(len(Servicio.objects.all()),1)
         print 'Verifica que no se cree un servicio con el nombre repetido'
         with self.assertRaises(DatabaseError):
             ServicioFactory(nombre='estoneado')
 
-class OrdenesDePedidoTestSuite(TestCase):
-    
-    capacidad_lavado = 200
-    servicio_estoneado_build = None
-    
-    def setUp(self):
-        self.servicio_estoneado = ServicioFactory(nombre='estoneado', lista=True)
-    
     def test_verificar_uniquetogheter_servicio_talla(self):
         servicio = ServicioFactory(lista=True)
         with self.assertRaises(DatabaseError):
@@ -78,15 +77,56 @@ class OrdenesDePedidoTestSuite(TestCase):
         dolares = round(float(self.servicio_estoneado.get_precio_dolares(talla=1,tipo_cambio=6.95)),2)
         bolivianos = round(float(self.servicio_estoneado.get_precio_bolivianos(talla=1,tipo_cambio=6.95)),2)
         self.assertEquals(dolares,round(bolivianos/6.95,2))
+
+class OrdenesDePedidoTestSuite(TestCase):
     
+    servicio_prelavado = None
+    servicio_estoneado = None
+    servicio_rasgado = None
+    
+    def setUp(self):
+        self.servicio_prelavado = ServicioFactory(nombre='pre lavado', lista=True)
+        self.servicio_estoneado = ServicioFactory(nombre='estoneado', lista=True)
+        self.servicio_rasgado = ServicioFactory(nombre='rasgado', lista=True)
+    
+    def test_registrar_orden_creacion_detalle_automatico(self):
+        print 'Verifica de una orden ingresada se creen las 3 instancia de DetalleOrden test de Ordenfactory'
+        orden = OrdenFactory(detalle=True)
+        self.assertEquals(DetalleOrden.objects.all().count(),3)
+
+    def test_registrar_orden_sin_creacion_detalle_automatica(self):
+        print 'Verifica de una orden ingresada se obtenga una lista sin instancias de DetalleOrden test de Ordenfactory'
+        orden = OrdenFactory(detalle=False)
+        self.assertEquals(DetalleOrden.objects.all().count(),0)
+
+    def test_obtener_lista_detalleorden_por_ejecutar(self):
+        print 'Verifica que se listen correctamente los servicios no termiandos'
+        orden = OrdenFactory(detalle=True)
+        self.assertEquals(DetalleOrden.noterminados.all().count(),3)
+        detalle = DetalleOrden.noterminados.all()[0]
+        detalle.terminado = True
+        detalle.save()
+        self.assertEquals(DetalleOrden.noterminados.all().count(),2)
+
+    def test_obtener_lista_detalleorden_por_ejecutar_por_fecha(self):
+        print 'dada una fecha con 2 ordenes con una se ocupa la capacidad'
+        orden1 = OrdenFactory(detalle=True, cantidad = 400)
+        orden2 = OrdenFactory(detalle=True, cantidad = 110)
+        self.assertEquals(DetalleOrden.noterminados.por_fecha_ejecucion().count(),6)
+        tipo = TipoServicio.objects.all()[0]
+        capacidad = DetalleOrden.noterminados.get_capacidad(fecha_ejecucion=orden1.fecha_entrega, tipo_servicio=tipo)
+        self.assertEqual(capacidad, 500)
+        #detalle1 = DetalleOrdenFactory(orden= orden, servicio=self.servicio_prelavado)
+        #detalle2 = DetalleOrdenFactory(orden= orden, servicio=self.servicio_estoneado)
+        #detalle3 = DetalleOrdenFactory(orden= orden, servicio=self.servicio_rasgado)
+        #detalle_orden = orden.get_detalle()
+        #self.assertIn(detalle1,detalle_orden)
+        #servicios = orden.get_lista_servicios()
+        #self.assertIn(self.servicio_prelavado, servicios)
+  
     def test_registrar_una_orden_de_pedido_menor_a_la_capacidad_empresa(self):
-        print 'Verifica la creacion de una orden de pedido para el tipo de servicio lavado con capacidad menor a la de la empresa, entrega en 1 dia'
-        #tplavado = TipoServicioFactory.build(titulo='Lavado', capacidad=self.capacidad_lavado)
-        #lavado = ServicioFactory.build(tipo_servicio=tplavado)
-        #op1 = OrdenPedidoFactory.build(servicio=[lavado], cantidad=self.capacidad_lavado)
-        #self.assertFalse(op1.tiene_capacidad_libre(timezone.now()))
-
-
-if __name__ == "__main__":
-    import unittest2 as unittest
-    unittest.main()
+        print 'Verifica la creacion de una orden de pedido con capacidad menor a la de la empresa'
+        #op1 = OrdenFactory.build(cantidad=self.capacidad_lavado)
+        #op1detalle = DetalleOrdenFactory(orden=op1, servicio=servicio_1)
+        #op1detalle = DetalleOrdenFactory(orden=op1, servicio=servicio_2)
+        #self.assertTrue(op1.tiene_capacidad_libre(timezone.now()))
