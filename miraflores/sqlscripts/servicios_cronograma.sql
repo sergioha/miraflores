ca0c50ca993a3cf1d84a4f9e3cbc5af48a959c0a
Sergio Hinojosa Avila <sergio.hinojosa.avila@gmail.com>
2013-01-18 23:43:56 -0400

    sql: views sql

---
+++ miraflores/sqlscripts/servicios_cronograma.sql
@@ -0,0 +1,3 @@
+﻿CREATE VIEW servicios_cronograma AS
+    SELECT detalle.id, cliente.user_id, orden.id AS orden_id, orden.cantidad, orden.talla, tipo.id AS tipo_servicio_id, tipo.nombre AS nombre_tiposervicio, servicio.id AS servicio_id, servicio.nombre AS nombre_servicio, detalle.fecha_ejecucion, detalle.terminado FROM servicios_detalleorden detalle, servicios_tiposervicio tipo, servicios_servicio servicio, servicios_orden orden, clientes_cliente cliente 
+    WHERE ((((servicio.tipo_servicio_id = tipo.id) AND (detalle.servicio_id = servicio.id)) AND (detalle.orden_id = orden.id)) AND (cliente.user_id = orden.cliente_id));
